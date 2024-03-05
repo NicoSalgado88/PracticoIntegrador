fa38bb52701fa00e3c44b91e678a0f5ab1566f57
FixedFun <54387710+FixedFun@users.noreply.github.com>
2024-03-05 18:53:03 -0300

    Sigo

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 arrayimg = ["imagenes/png-jpg/edificio2.jpg", "imagenes/png-jpg/Ballena Beluga.jpg",
-"imagenes/png-jpg/edificio1.png"]
+    "imagenes/png-jpg/edificio1.png"]
 i = 0
 
 function imgchange(num) {
     console.log(arrayimg.length)
     console.log(num)
     let img1 = document.getElementById("imgcrs")
-switch (num) {
-    case 1:
-        i++
-        if (i > arrayimg.length-1) {
-            i = 0
-        }
-        img1.src = arrayimg[i]
-        console.log("index: " + i)
-        break;
-    case -1:
-        i--
-        if (i == -1) {
-            i = 2
-        }
-        img1.src = arrayimg[i]
-        console.log("index: " +  i)
-        break;
+    switch (num) {
+        case 1:
+            i++
+            if (i > arrayimg.length - 1) {
+                i = 0;
+            }
+            img1.src = arrayimg[i]
+            break;
+        case -1:
+            i--
+            if (i == -1) {
+                i = 2
+            }
+            img1.src = arrayimg[i]
+            break;
+    }
 }
+
+function imgchooser(num2) {
+    let img1 = document.getElementById("imgcrs")
+    img1.src = arrayimg[num2]
+}
+
+function imgt() {
+    console.log("ala")
+    for (let j = 0; j < arrayimg.length; j++) {
+        let img2 = document.getElementById("thumbnailcrs")
+        let img3 = document.createElement("img")
+        img3.setAttribute("onclick", `imgchooser(${j})`)
+        console.log(img3)
+        img3.src = arrayimg[j]
+        img2.appendChild(img3)
+    }
 }
